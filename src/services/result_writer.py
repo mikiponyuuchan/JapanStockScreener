a8b32e7c18e5a325be6b2039a0bc0a9eac561ec1
mikiponyuuchan <mikipon.yuuchan@gmail.com>
2026-08-05 08:05:55 +0900

    Ver3.3 Excelレポート化

--- src/services/result_writer.py
+++ src/services/result_writer.py
@@ -3,49 +3,65 @@ from datetime import datetime
 from pathlib import Path
 
 
-
 def save_result(df):
 
-
     # 保存フォルダ
-
     folder = Path("results")
-
-    folder.mkdir(
-        exist_ok=True
-    )
-
-
+    folder.mkdir(exist_ok=True)
 
     # 日付
+    today = datetime.now().strftime("%Y-%m-%d")
 
-    today = (
-        datetime.now()
-        .strftime("%Y-%m-%d")
-    )
+    csv_file = folder / f"{today}_stock_result.csv"
+    excel_file = folder / f"{today}_stock_result.xlsx"
 
-
-
-    filename = (
-        folder
-        /
-        f"{today}_stock_result.csv"
-    )
-
-
-
-    # 保存
-
+    # -----------------------------
+    # CSV保存
+    # -----------------------------
     df.to_csv(
-        filename,
+        csv_file,
         index=False,
         encoding="utf-8-sig"
     )
 
+    # -----------------------------
+    # Excel保存
+    # -----------------------------
+    with pd.ExcelWriter(
+        excel_file,
+        engine="openpyxl"
+    ) as writer:
 
+        # 全銘柄
+        df.to_excel(
+            writer,
+            sheet_name="全銘柄",
+            index=False
+        )
+
+        # TOP20
+        top20 = df.sort_values(
+            "強気度",
+            ascending=False
+        ).head(20)
+
+        top20.to_excel(
+            writer,
+            sheet_name="TOP20",
+            index=False
+        )
+
+        # 買い候補
+        buy_df = df[
+            df["総合判定"] == "買い候補"
+        ]
+
+        buy_df.to_excel(
+            writer,
+            sheet_name="買い候補",
+            index=False
+        )
 
     print()
-    print(
-        "結果保存:",
-        filename
-    )
+    print("CSV保存 :", csv_file)
+    print("Excel保存 :", excel_file)
